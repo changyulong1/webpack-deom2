2e539b336694adabbbb3963e3f9dbdc9f61f099f
xiaocao <853044263@qq.com>
2022-02-04 12:37:22 +0800

    webpack-dev-server 更新

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     entry: './src/index.js',
     output: {
         filename: '[name].[chunkhash].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '常玉龙',
+            title: '我是首页',
+            template: 'src/assets/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
 };
